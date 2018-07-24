84c06aca7c3090895b46181d6bd6ef99bb9b5320
zanzes <nikolajea@gmail.com>
2018-07-24 05:34:23 +0200

    split

--- ScratchPad.py
+++ ScratchPad.py
@@ -5,6 +5,7 @@ from functools import partial
 from lztools.Bash import return_command_result
 from lztools.Data.Images import get_random_image, search
 from lztools.Debugging import time_operation
+from lztools.text import box
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -19,8 +20,9 @@ def pi(imgs):
 
 if __name__ == '__main__':
     # y = subprocess.check_output(["echo", "123"])
-    x = return_command_result("echo", "123\nass")
-    print(x)
+    # x = return_command_result("echo", "123\nass")
+    # print(x)
+    box("lol")
 
 
 
