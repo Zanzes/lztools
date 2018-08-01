d5a396449c29eef162d034b5235d81d9d8307a00
zanzes <nikolajea@gmail.com>
2018-08-01 11:10:41 +0200

    ColumnWriter v1.1

--- lztools/cli/preg.py
+++ lztools/cli/preg.py
@@ -3,7 +3,7 @@ from lztools.text import regex
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-@click.command(context_settings=CONTEXT_SETTINGS)
+@click.group(context_settings=CONTEXT_SETTINGS)
 @click.argument("expr")
 @click.argument("text", default=click.get_text_stream('stdin'))
 @click.option("-s", "--single-result", is_flag=True, default=False)
--- setup.py
+++ setup.py
@@ -31,12 +31,12 @@ setup(
             'lztools = cli.console:main',
             'lmod = cli.lmod:main',
             'ldoc = cli.ldock:main',
-            'preg = cli.preg:main'
+            'preg = lztools.cli.preg:main'
             'lgit = cli.lgit:main'
         ],
     },
     install_requires=pip_requires,
-    packages=['lztools', 'lztools.Managers', 'lztools.DataTypes', 'lztools.Data', 'lztools.Junk', 'Resources', "Commands", "cli"],
+    packages=['lztools', 'lztools.Managers', 'lztools.DataTypes', 'lztools.Data', 'lztools.Junk', 'lztools.cli', 'Resources'],
     zip_safe=False,
     classifiers=[  # Optional
         # How mature is this project? Common values are
@@ -54,12 +54,12 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        # 'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.7'  # ,
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.4',
-        # 'Programming Language :: Python :: 3.5',
-        # 'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'  # ,
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
 )
