be9058151a9b722e709304eb41116f3e15d9c42d
zanzes <nikolajea@gmail.com>
2018-08-16 08:35:13 +0200

    dumb password removed

--- storage/setup.py
+++ storage/setup.py
@@ -14,7 +14,7 @@ setup(
     name='lztools.storage',
     author='Laz aka Zanzes',
     author_email='ubuntuea@gmail.com',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT License',
     description='Template',
     url='',
