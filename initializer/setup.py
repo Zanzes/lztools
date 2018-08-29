09cef23ce9330d26a516845f55eaf5a4d14888b0
zanzes <nikolajea@gmail.com>
2018-08-29 06:28:32 +0200

    MIRS-7392 bashrc

--- initializer/setup.py
+++ initializer/setup.py
@@ -24,7 +24,7 @@ setup(
         ],
     },
     install_requires=pip_requires,
-    packages=['lztools'],
+    packages=['lztools', 'pyres'],
     zip_safe=False,
     include_package_data=True,
     classifiers=[  # Optional
