ac948dab415ccaab69ba0743533a1ec668be4163
zanzes <nikolajea@gmail.com>
2018-08-31 01:34:23 +0200

    versioning

--- initializer/setup.py
+++ initializer/setup.py
@@ -15,7 +15,7 @@ setup(
     name='lztools.initializer',
     author='Laz aka Zanzes',
     author_email='ubuntuea@gmail.com',
-    version='1.0.5',
+    version='1.0.6',
     license='MIT License',
     description='Template',
     url='',
