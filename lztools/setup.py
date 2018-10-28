42c3c8a23a435000ec8e29c27dd5db3a0ea68277
zanzes <nikolajea@gmail.com>
2018-10-28 08:08:29 +0100

    Fun fixed up

--- lztools/setup.py
+++ lztools/setup.py
@@ -15,7 +15,7 @@ from setup_requires import apt_requires, pip_requires
 #     codecs.register(func)
 
 setup(
-    name='lztools.core',
+    name='lztools',
     author='Laz aka Zanzes',
     author_email='ubuntuea@gmail.com',
     version='1.2.18',
