44c594ab7b1bb82d9f54989c5bf23a09de3c1b41
zanzes <nikolajea@gmail.com>
2019-10-05 05:28:54 +0200

    Changed subsystem default

--- lztools/xplatform.py
+++ lztools/xplatform.py
@@ -3,5 +3,5 @@ from lztools import linux, windows
 def execute(on_linux:exec, on_windows:exec, subsystem_valid:bool=False):
     if linux.is_active_system(subsystem_valid):
         return on_linux()
-    elif windows.is_active_system(subsystem_valid):
+    elif windows.is_active_system(not subsystem_valid):
         return on_windows()
